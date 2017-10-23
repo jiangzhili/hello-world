a44009fb562bbed185579398af68175e1b57e2a7
jiangzhili <jiangzhili@thunisoft.com>
2017-10-23 15:06:58 +0800

    Initial commit

---
+++ src/com/idea/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.idea.demo;
+
+/**
+ * Created by henleyjiang on 2017/10/23.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
